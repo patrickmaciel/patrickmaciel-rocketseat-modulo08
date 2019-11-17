d847d92b8072db70a8d391df2b07860dc41408e9
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-11-17 16:40:39 -0300

    hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import GlobalStyle from './styles/global';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState(['React.js', 'React Native']);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
+    setNewTech('');
   }
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // willUnmount
+    // deixa de ser montado - deixar de existir
+    // return () => {};
+  }, []);
+
+  // função que vai ser executada
+  // quando vai ser executada = array de dependências
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]); // o state que você for usar deve estar nesse array
 
   return (
     <>
@@ -20,7 +39,9 @@ function App() {
       </ul>
 
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
-      <button onClick={handleAdd}>Adicionar</button>
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
     </>
   );
 }
